2d74836b22f55b30a3be143c9a4a111395c21c06
netcan <1469709759@qq.com>
2017-11-08 19:17:47 +0800

    改写试题数据结构

--- HFUT_Exam_Crawler_Py/gen_doc.py
+++ HFUT_Exam_Crawler_Py/gen_doc.py
@@ -1,5 +1,6 @@
 import sqlite3
 from DESCipher import DESCipher
+from collections import namedtuple
 import os
 
 
@@ -9,35 +10,37 @@ def gen_doc(db_name='exam_somarx.db', password=None):
     cipher = DESCipher(password) if password else None
     db = sqlite3.connect(db_name)
     courses = ['mzdsxhzgteshzylltx', 'marxism', 'zgjdsgy', 'sxddxyyfljc']
-    fields = ['chapter', 'type', 'subject', 'a', 'b', 'c', 'd', 'e', 'answer']
+    # 没有方法的类，只有属性
+    Question = namedtuple('Question', ['chapter', 'type', 'subject', 'a', 'b', 'c', 'd', 'e', 'answer'])
     sql = 'SELECT chapter, type, subject, a, b, c, d, e, answer FROM {} ORDER BY ABS(chapter), ABS(type);'
     types = [None, '判断题', '单选题', '多选题']
     for course in courses:
         md = ''
-        qs = [dict(zip(fields, q)) for q in
+        qs = [Question(*q) for q in
               db.execute(sql.format(course)).fetchall()]
         chapter = -1
         tye = -1
 
         for q in qs:
-            if chapter != int(q['chapter']):
-                chapter = int(q['chapter'])
+            if chapter != int(q.chapter):
+                chapter = int(q.chapter)
                 md += '## 第 {} 章\n'.format(chapter)
-            if tye != q['type']:
-                tye = q['type']
+            if tye != q.type:
+                tye = q.type
                 md += '\n### {}\n'.format(types[tye])
                 qid = 1
 
-            md += "{}. {} **{}**\n".format(qid, (cipher.decrypt(q['subject']) if cipher else q['subject']).strip(),
-                                           q['answer'].replace('0', '错').replace('1', '对')
-                                           if q['answer'] in '01' else q['answer'])
+            md += "{}. {} **{}**\n".format(qid, (cipher.decrypt(q.subject) if cipher else q.subject).strip(),
+                                           q.answer.replace('0', '错').replace('1', '对')
+                                           if q.answer in '01' else q.answer)
             # 选择题
             if tye != 1:
                 md += '```\n'
                 for s in list('abcde'):
-                    if q[s]:
+                    sel = q._asdict()[s]
+                    if sel:
                         md += '{}. {}\n'.format(s.upper(),
-                                                (cipher.decrypt(q[s]) if cipher else q[s]).strip())
+                                                (cipher.decrypt(sel) if cipher else sel).strip())
                 md += '```\n'
             qid += 1
             with open('output/{}.md'.format(course), 'w') as f:
